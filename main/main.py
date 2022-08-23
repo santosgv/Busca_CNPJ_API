e990113447e901a9f96e4b862afa9ce9d8e2348e
Vitor.Gomes <santosgomesv@gmail.com>
2022-08-23 00:00:20 -0300

    api busca CNPJ finalizada !

--- main/main.py
+++ main/main.py
@@ -1,7 +1,50 @@
-from fastapi import fastapi
+from fastapi import FastAPI
+from decouple import config
+import requests
 
-app= fastapi()
+app= FastAPI()
 
 @app.get("/")
-def root():
-    return {'mensagem':'teste'}
+def ROOT():
+    return {'mensagem':'home'}
+
+@app.post("/cnpj")
+def CNPJ(cnpj:int):
+    cnpj = cnpj
+    url = 'https://api.infosimples.com/api/v2/consultas/receita-federal/simples-dasn'
+    args = {
+    "cnpj":    cnpj,
+    "token":  config('token'),
+    "timeout": 300
+    }
+
+    response = requests.post(url, args)
+    response_json = response.json()
+    response.close()
+
+    if response_json['code'] == 200:
+        print("Retorno com sucesso: ", response_json['data'])
+        dados = response_json['data']
+
+        cnpj = dados[0]['cnpj']
+        razao_social = dados[0]['razao_social']
+        anos =dados[0]['original']
+
+        return {'cnpj':cnpj,
+                'razao_social':razao_social,
+                'ano':anos
+                }
+            
+        
+
+
+    elif response_json['code'] in range(600, 799):
+        mensagem = "Resultado sem sucesso. Leia para saber mais: \n"
+        mensagem += "Código: {} ({})\n".format(response_json['code'], response_json['code_message'])
+        mensagem += "; ".join(response_json['errors'])
+        print(mensagem)
+
+    print("Cabeçalho da consulta: ", response_json['header'])
+    print("URLs com comprovantes (HTML/PDF): ", response_json['site_receipts'])
+
+    return {'mensagem':mensagem}
