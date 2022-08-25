08670c63440afe04a547cfc6bd2299078990b3c5
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2022-08-25 15:49:24 -0300

    conf uvicorn

---
+++ main/uvicorn.py
@@ -0,0 +1,9 @@
+import uvicorn
+
+
+    ...
+
+if __name__ == "__main__":
+    config = uvicorn.Config("main:app", port=5000, log_level="info")
+    server = uvicorn.Server(config)
+    server.run()
